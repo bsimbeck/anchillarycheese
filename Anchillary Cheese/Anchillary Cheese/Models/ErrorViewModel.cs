877cee2d89419ccdd0fce17cada7e75e9e1e5817
Branden Simbeck <branden@brandensimbeck.com>
2022-01-11 22:43:06 -0500

    Inital Commit

---
+++ Anchillary Cheese/Anchillary Cheese/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Anchillary_Cheese.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
